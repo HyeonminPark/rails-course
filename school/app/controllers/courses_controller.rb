f481ca33878716f5da8b71f6064ee72c405cc745
Hyeonmin Park <hpark6@my.waketech.edu>
2019-01-30 03:08:20 +0000

    Class - One to Many

---
+++ school/app/controllers/courses_controller.rb
@@ -0,0 +1,45 @@
+class CoursesController < ApplicationController
+    #CRUD
+    #send back form to create
+    def new
+        @course = Course.new
+    end
+    
+    # create course
+    
+    def create
+        course = Course.new
+        course.name = params[:name]
+        course.location =params[:location]
+        course.description =params[:description]
+        course.save
+        redirect_to '/courses'
+    end
+    #list all courses
+    def index
+        @courses = Course.all
+    end
+    #send back form to edit
+    def edit
+        @course = Course.find(params[:id])
+    end
+    #updates course
+    def update
+        course = Course.find(params[:id])
+        course.name = params[:name]
+        course.location =params[:location]
+        course.description =params[:description]
+        course.save
+        redirect_to '/courses'
+    end
+    #destroy
+    def destroy
+        course = Course.find(params[:id])
+        course.destroy
+        redirect_to '/courses'
+    end
+    
+    def show
+        @course = Course.find(params[:id])
+    end
+end
