b210d4061892f0db7d2c3f8192f791f466a83cd9
Hyeonmin Park <hpark6@my.waketech.edu>
2019-02-13 23:08:29 +0000

    restaurant update

---
+++ restaurant/app/controllers/dishes_controller.rb
@@ -0,0 +1,44 @@
+class DishesController < ApplicationController
+    def index
+        @dishes = Dish.all
+    end
+    
+    def new
+        @dish = Dish.new
+    end
+    
+    def create
+        dish = Dish.new
+        dish.name = params[:dish][:name]
+        dish.description = params[:dish][:description]
+        dish.picture = params[:dish][:picture]
+        dish.price = params[:dish][:price]
+        dish.save
+        redirect_to dishes_path
+    end
+    
+    def edit
+        @dish = Dish.find(params[:id])
+    end
+    
+    # HTML
+    # <input name="dish[name]">
+    # param => { dish: { name: 'idfd' }
+    # param[:dish][:name]
+    
+    def update
+        dish = Dish.find(params[:id])
+        dish.name = params[:dish][:name]
+        dish.description = params[:dish][:description]
+        dish.picture = params[:dish][:picture]
+        dish.price = params[:dish][:price]
+        dish.save
+        redirect_to dishes_path
+    end
+    
+    def delete
+        dish = Dish.find(params[:id])
+        dish
+    end
+    
+end
