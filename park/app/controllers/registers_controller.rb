ddec1d83cbac2efb9dba0776c4228bac0364c0d2
Hyeonmin Park <hpark6@my.waketech.edu>
2019-03-19 00:54:08 +0000

    changed

--- park/app/controllers/registers_controller.rb
+++ park/app/controllers/registers_controller.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 class RegistersController < ApplicationController
- before_action :authenticate_user!
- 
+  before_action :authenticate_user!
+
   def index
     @registers = Register.where(user_id: current_user.id)
   end
-  
+
   def show
     @register = Register.find(params[:id])
     @activities = Activity.all
   end
-  
+
   def new
     @register = Register.new
     @activities = Activity.all
   end
-  
+
   def create
     register = Register.new
     register.user_id = current_user.id
@@ -29,13 +31,12 @@ class RegistersController < ApplicationController
     register.picture = params[:register][:picture]
     register.save
     redirect_to registers_path
-    
   end
-  
+
   def edit
     @register = Register.find(params[:id])
   end
-  
+
   def update
     register = Register.find(params[:id])
     register.user_id = current_user.id
@@ -51,19 +52,19 @@ class RegistersController < ApplicationController
     register.save
     redirect_to registers_path
   end
-  
+
   def destroy
     register = Register.find(params[:id])
     register.destroy
     redirect_to registers_path
   end
-  
+
   def add_activity
-    register = Register.find(params[:register_id])
+    @register = Register.find(params[:register_id])
     register_activity = RegisterActivity.new
     register_activity.register_id = params[:register_id]
     register_activity.activity_id = params[:activity_id]
     register_activity.save
-    redirect_to registers_path(register)
+    redirect_to registers_path(@register)
   end
 end
